2e21d94411a5b6db322c98bc3f25d1cf332703f1
devSaakar <saakarchauhan@gmail.com>
2024-12-10 14:14:49 +0530

    ft: removed extra code.

--- src/App.tsx
+++ src/App.tsx
@@ -7,7 +7,7 @@ function App() {
     <Router>
       <Routes>
         <Route path="/" element={<UserRepo />} />
-        <Route path="/login" element={<Login />} />
+        {/* <Route path="/login" element={<Login />} /> */}
       </Routes>
     </Router>
   );
